ebd2e9344afcffb48491814539efe18357a5dd7f
melodyyun <melodyyunc@gmail.com>
2018-01-23 19:33:08 -0500

    update lesson 1.3

--- Week 2/Lesson 1.3/Exercise/1.3.js
+++ Week 2/Lesson 1.3/Exercise/1.3.js
@@ -9,11 +9,17 @@ const num3 = 5;
 //function delaration:
 function add(num1, num2, num3){
 	//instructions
-	const sum = num1 + num2 + num3;
-	console.log(`add all the things and we have ${num1}, ${num2}, ${num3},`);
-	console.log(`sum = ${sum}`);
-	return sum;
+	console.log(`add all the things and we have ${num1}, ${num2}, ${num3}`);
+	console.log(`sum = ${num1} + ${num2} + ${num3}`);
+	return num1 + num2 + num3;
 }
+
+const sum = add(5, 5, 5);
+console.log(`Sum is ${sum}`);
+
+//add 4 numbers without changing the previous function
+const newSum = add (1, add(1,1,1));
+console.log (`New sum is ${newSum}`);
 
 add(2, 4, 6);
 
@@ -34,7 +40,8 @@ const helloFriend = (firstName = "Jane", lastName = "Doe", age = 1, favFood = "i
 
 helloFriend("Melody", "Chen", undefined, "Chinese food"); // age will = 1
 
-const addMore = (number) => console.log(number);
-
+const addMore = number => console.log(number);
+// if there is only one parameter then no brackets needed 
+// if you have more than one or none use  ()
 
 addMore(5);
