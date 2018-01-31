19b55b99f5f069f26bb2844b2e084ea2d53da35b
melodyyun <melodyyunc@gmail.com>
2018-01-30 20:57:47 -0500

    lesson 5 array and array methods

---
+++ Week 3/Lesson 1.5/Exercise/ArrayMethods.js
@@ -0,0 +1,50 @@
+const pizzaToppings = [
+    'cheese',
+    'pepperoni',
+    'sauce',
+]
+
+console.log(pizzaToppings);
+
+//push add something to the end
+pizzaToppings.push('olives');
+console.log(pizzaToppings);
+
+//unshift add something to the beginning
+pizzaToppings.unshift('dough');
+console.log(pizzaToppings);
+
+//pop removes something from the end
+pizzaToppings.pop();
+console.log(pizzaToppings);
+
+//shift removes something from the beginning
+pizzaToppings.shift();
+console.log(pizzaToppings);
+
+const names = [];
+
+names.push('Sam');
+names.push('Alex');
+names.push('Jess');
+
+console.log(names);
+
+names.unshift('Chris');
+
+console.log(names);
+
+const spiceGirls = ["Ginger Spice", "Scary Spice", "Baby Spice", "Sporty Spice", "Posh Spice"];
+
+spiceGirls.shift();
+console.log(spiceGirls);
+spiceGirls.pop();
+console.log(spiceGirls);
+
+const newArray = spiceGirls.concat(["Fennel Spice", 'Old Spice'], ['Basil Spice']);
+console.log(newArray);
+
+const sportyIndex = newArray.indexOf('Sporty Spice');
+console.log(sportyIndex); // returns 2
+
+console.log(newArray[sportyIndex]); // newArray[2];
