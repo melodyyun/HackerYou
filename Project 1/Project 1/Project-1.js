fe69d41f4d40c6a674663de4a0638400df1cd574
melodyyun <melodyyunc@gmail.com>
2018-02-15 19:24:24 -0500

    Update project 1

---
+++ Project 1/Project 1/Project-1.js
@@ -0,0 +1,20 @@
+//----------------
+//Countdown Timer
+//----------------
+
+//document.ready
+(function() {
+
+    // const set = document.querySelector(input[type=submit]);
+    // set.addEventListener('submit', function (e) {
+    //     e.preventDefault();
+    //     console.log('clicked submit');
+    // })
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const $totalMinutes = $('input[type=number]')
+    });
+
+//Runs checkTime every second
+// setTimeout(checkTime, 1000);
+})();
