9ddbd484e6c777e169012c4704cdd648042fbdbb
melodyyun <melodyyunc@gmail.com>
2018-02-27 21:13:28 -0500

    grocery list project

---
+++ Js-advanced/Week 7/Lesson 2.1/Best Practices and code organization and scope/main.js
@@ -0,0 +1,28 @@
+let planet = "earth";
+const travel = () =>{
+    planet = "mars";
+    console.log(planet);
+}
+travel();
+console.log(planet);
+
+
+const space = () =>{
+    let stars = 1000;
+}
+
+
+const myApp = {
+    twitter: "thisishackeryou",
+};
+
+// myApp.twitter = 'thisishackeryou';
+
+console.log(myApp.twitter);
+
+
+//document.ready
+$(function () {
+    //will start widgetApp on initial start
+    widgetApp.init();
+});
